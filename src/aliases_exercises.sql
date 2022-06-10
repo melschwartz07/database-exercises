5cf113cd2ed6fd6c7ecfefd9486bdeba22322163
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-10 11:14:56 -0500

    aliases exercise complete

---
+++ src/aliases_exercises.sql
@@ -0,0 +1,10 @@
+-- Use the employees database.
+-- Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
+--
+-- Add the date of birth for each employee as 'DOB' to the query.
+--
+-- Update the query to format full name to include the employee number so it is formatted as 'employee number - last name, first name'.
+
+select concat(emp_no, " ", "-" , " ", last_name, ", ", first_name) as "full_name", birth_date as "DOB"
+from employees
+         LIMIT 10;
