f21cbb592b11ee6f67e3fa88ae697f72240c7c36
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-10 09:40:05 -0500

    select exercises complete

--- src/select_exercises.sql
+++ src/select_exercises.sql
@@ -1,11 +1,10 @@
-
+create database if not exists codeup_test_db;
 use codeup_test_db;
 
 
 select album_name from albums where artist_name = 'Pink Floyd';
 select release_date from albums where album_name = 'Sgt. Peppers lonely Hearts Club Band';
 select genre from albums where album_name = 'Nevermind';
-select album_name from albums where release_date = '1990';
-select album_name from albums where sales < '20 million';
+select album_name from albums where release_date = '1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999';
+select album_name from albums where sales < 20;
 select album_name from albums where genre = 'Rock';
-
